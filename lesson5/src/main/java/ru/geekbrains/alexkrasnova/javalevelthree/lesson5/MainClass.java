306eea10cd06985b9346c5d4f6f354cd90f6e2f7
Alexandra Krasnova <krasnova@physics.msu.ru>
2021-04-11 01:41:13 +0300

    Сделана синхронизация потоков с использованием CountDownLatch и Semaphore.

---
+++ lesson5/src/main/java/ru/geekbrains/alexkrasnova/javalevelthree/lesson5/MainClass.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.alexkrasnova.javalevelthree.lesson5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static CountDownLatch startRaceLatch;
+    public static CountDownLatch endRaceLatch;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        startRaceLatch = new CountDownLatch(CARS_COUNT);
+        endRaceLatch = new CountDownLatch(CARS_COUNT);
+
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        try {
+            MainClass.startRaceLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        try {
+            MainClass.endRaceLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
